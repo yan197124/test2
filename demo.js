d680cf90fced73a800170247eafec32527c93ac2
runoob <1971242342@qq.com>
2024-10-09 09:55:18 +0800

    commit demo2.js

---
+++ demo.js
@@ -0,0 +1,6 @@
+function fun1(){
+	console.log("hello");
+	var a = 123;
+	console.log(a);
+}
+fun1();
---
+++ demo2.js
@@ -0,0 +1,7 @@
+function fun1(){
+	var a = 1;
+	var b = 2;
+	var c = a + b;
+	console.log(c);
+}
+fun1();
